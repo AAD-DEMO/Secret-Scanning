ce9c6d3b36ca63e86f0d14b258defc7577d32458
S Sunil deep <109170070+Sunil0916@users.noreply.github.com>
2023-06-16 22:00:16 +0530

    Update secret.py

--- secret.py
+++ secret.py
@@ -1,3 +1,7 @@
 secret_key = "my_secret_key"
 password = "password123"
 api_key = "another_secret_key"
+[default]
+aws_access_key_id = AKIAIOSFODNN7EXAMPLE
+aws_secret_access_key = wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
+
